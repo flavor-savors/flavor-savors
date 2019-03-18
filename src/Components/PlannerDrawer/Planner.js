ead99450d132fc4698ad873992549de10b71da3c
Candace <candacedowning@gmail.com>
2019-03-18 14:57:08 -0500

    more framework added

---
+++ src/Components/PlannerDrawer/Planner.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Planner extends Component {
+
+
+    render(){
+        return(
+            <div>
+                Placeholder text for the meal planning drawer
+            </div>
+        )
+    }
+}
+
+export default Planner
--- src/Components/Recipes/SmallRecipe/SmallRecipe.js
+++ src/Components/Recipes/SmallRecipe/SmallRecipe.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import LargeRecipe from '../LargeRecipe/LargeRecipe';
 
 //this component is the map for rendering the small recipe cards after a specific call is made for:
     //all recipes public
@@ -6,9 +7,17 @@ import React, {Component} from 'react'
     //users private
     //specific query
     //with filters
+//And renders the large recipe card when a specific recipe is clicked 
 
 
 class SmallRecipe extends Component {
+    constructor(props){
+        super(props)
+
+        this.state ={
+            largeRecipe: false
+        }
+    }
 
     render(){
 
@@ -16,22 +25,37 @@ class SmallRecipe extends Component {
         // const recipes = placeholderArray.map((e, i)=>{
         //     return(
         //         <Link>
-        //         <div key={i} style={background-image:{e.image}}>
-        //             <h1>{e.title}</h1>
-        //             <div>{e.ingredients}</div>    
-        //             <div>{e.tags}</div>
-        //             <div>   
-        //                  <button>add to favs</button>
-        //                  <button>add to meal plan</button>
-        //             </div>
-        //         </div>  
+                // <div key={i} style={background-image:{e.image}}>
+                //     <h1>{e.title}</h1>
+                //     <div>{e.ingredients}</div>    
+                //     <div>{e.tags}</div>
+                //     <div>   
+                //          <button>add to favs</button>
+                //          <button onClick={this.props.togglePlanner}>add to meal plan</button>
+                //     </div>
+                // </div>  
         //         </Link>              
         //     )
         // })
 
         return(
             <div>
+                {!this.state.largeRecipeShow ? 
+                    null : 
+                    //need to add code to pass individual id's to this component when user clicks
+                    <LargeRecipe/> }
+                
                 {/* {recipes} */}
+                {/* the following is placeholder code until functionality is established */}
+                <div>
+                    <h1>title</h1>
+                    <div>ingredients</div>    
+                    <div>tags</div>
+                    <div>   
+                         <button>add to favs</button>
+                         <button onClick={this.props.togglePlanner}>add to meal plan</button>
+                    </div>
+                </div>  
             </div>
         )
     }
