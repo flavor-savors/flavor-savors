9ed54d723a97afd6e25c6262b8c3027a28dea695
Uriel Meneses <saidmeneses1@live.com>
2019-03-27 12:25:49 -0500

    func for components

--- src/Components/Header/Register/Register.js
+++ src/Components/Header/Register/Register.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 
-class Register extends Component {
-    constructor(){
-        super();
-        this.state = {
+
+const intialState ={
             email: '',
             password: '',
             displayName: '',
             photoURL: '',
-            info: []
-        }
-    }
+            info: [],
+            emailErr: '',
+            passErr: '',
+            nameErr: '',
+            imgErr: ''
+}
+
+class Register extends Component {
+    state = intialState;
 
     handleRegister = (e) => {
         e.preventDefault()
@@ -21,11 +25,42 @@ class Register extends Component {
         .then(response => {
             this.setState({info: response.data})
         })
+        const isValid = this.validate();
+        if(isValid){
+          this.setState({intialState})
+          this.props.t();
+        }
     }
 
     handleRegInputs = (e) => {
         this.setState({[e.target.name]: e.target.value})
     }
+
+    validate = () => {
+      let nameErr = '';
+      let emailErr = '';
+      let passErr = '';
+      let imgErr = '';
+
+      if(!this.state.email.includes('@')){
+          emailErr = 'Invalid Email'
+      }
+      if(this.state.password.length < 6){
+          passErr = 'Needs to be longer 6 characters'
+      }
+      if(!this.state.displayName){
+          nameErr = "Name can't be empty"
+      }
+      if(!this.state.photoURL){
+        imgErr = 'You need a profile image'
+      }
+
+      if(emailErr || passErr || nameErr || imgErr){
+          this.setState({emailErr, passErr, nameErr, imgErr})
+          return false
+      }
+      return true;
+  }
 
 
   render() {
@@ -38,24 +73,32 @@ class Register extends Component {
             onChange={this.handleRegInputs}
             className='reg-ipt'
           />
+          <small className='reg-err'>{this.state.nameErr}</small>
+
           <input
             placeholder='Email'
             name='email'
             onChange={this.handleRegInputs}
             className='reg-ipt'
           />
+          <small className='reg-err'>{this.state.emailErr}</small>
+
           <input
             placeholder='Password'
             name='password'
             onChange={this.handleRegInputs}
             className='reg-ipt'
           />
+          <small className='reg-err'>{this.state.passErr}</small>
+
           <input
             placeholder='Photo'
             name='photoURL'
             onChange={this.handleRegInputs}
             className='reg-ipt'
           />
+          <small className='reg-err'>{this.state.imgErr}</small>
+
           <button className='regsub' type='submit' value='Submit'>Submit</button>
       </form>
     )
--- src/Components/Tutorial/Tutorial.js
+++ src/Components/Tutorial/Tutorial.js
@@ -6,7 +6,9 @@ class Tutorial extends Component {
   render() {
     return (
       <div className='tut-div'>
-          <div className='tut-steps'> browse recipes</div>
+          <div className='tut-steps'> <p>browse recipes</p>
+            <img className='gifs' src='https://media.giphy.com/media/APq4KTnluN8E6phP5U/giphy.gif' alt='vid'/>
+          </div>
           <div className='tut-steps'> createna plan</div>
           <div className='tut-steps'> sign up and save plans</div>
       </div>
