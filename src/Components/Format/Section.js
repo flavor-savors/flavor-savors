7011d67cce0835fc3155c5d08b023bc16b662f5d
Candace <candacedowning@gmail.com>
2019-03-29 11:43:27 -0500

    safety commit

--- src/Components/Format/Section.js
+++ src/Components/Format/Section.js
@@ -1,111 +1,111 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Section extends Component {
-	render() {
-		console.log('section props', this.props)
-		return (
-			<div className='sec-2'>
-				{this.props.plan.map((meal, index) => {
-					if (meal.code.includes(this.props.day)) {
-						return (
-							<section>
-								<div>
-									{/* <p>breakfast</p> */}
-									{this.props.plan[index].code[0] === 'b' ? (
-										<div className='meal'>
-											{this.props.plan[index].code[0] === 'b' ? (
-												<p>breakfast</p>
-											) : this.props.plan[index].code[0] === 'l' ? (
-												<p>lunch</p>
-											) : this.props.plan[index].code[0] === 'd' ? (
-												<p>dinner</p>
-											) : this.props.plan[index].code[0] === 's' ? (
-												<p>snacks</p>
-											) : (
-												<p>what did you do</p>
-											)}
-											{this.props.plan[index].recipe.recipeName}
-											{this.props.plan[index].recipe.ingredient.map((ing) => (
-												<div>
-													<h1>ingredients</h1>
-													<div>{ing.name}</div>
-													<div>{ing.amount}</div>
-												</div>
-											))}
-											{this.props.plan[index].recipe.directions}
-										</div>
-									) : this.props.plan[index].code[0] === 'l' ? (
-										<div className='meal'>
-											{this.props.plan[index].code[0] === 'b' ? (
-												<p>breakfast</p>
-											) : this.props.plan[index].code[0] === 'l' ? (
-												<p>lunch</p>
-											) : this.props.plan[index].code[0] === 'd' ? (
-												<p>dinner</p>
-											) : this.props.plan[index].code[0] === 's' ? (
-												<p>snacks</p>
-											) : (
-												<p>what did you do</p>
-											)}
-											{this.props.plan[index].recipe.recipeName}
-											{this.props.plan[index].recipe.ingredient.map((ing) => (
-												<div>
-													<h1>ingredients</h1>
-													<div>{ing.name}</div>
-													<div>{ing.amount}</div>
-												</div>
-											))}
-											{this.props.plan[index].recipe.directions}
-										</div>
-									) : this.props.plan[index].code[0] === 'd' ? (
-										<div className='meal'>
-											{this.props.plan[index].code[0] === 'b' ? (
-												<p>breakfast</p>
-											) : this.props.plan[index].code[0] === 'l' ? (
-												<p>lunch</p>
-											) : this.props.plan[index].code[0] === 'd' ? (
-												<p>dinner</p>
-											) : this.props.plan[index].code[0] === 's' ? (
-												<p>snacks</p>
-											) : (
-												<p>what did you do</p>
-											)}
-											{this.props.plan[index].recipe.recipeName}
-											{this.props.plan[index].recipe.ingredient.map((ing) => (
-												<div>
-													<h1>ingredients</h1>
-													<div>{ing.name}</div>
-													<div>{ing.amount}</div>
-												</div>
-											))}
-											{this.props.plan[index].recipe.directions}
-										</div>
-									) : this.props.plan[index].code[0] === 's' ? (
-										<div className='meal'>
-											{this.props.plan[index].code[0] === 'b' ? (
-												<p>breakfast</p>
-											) : this.props.plan[index].code[0] === 'l' ? (
-												<p>lunch</p>
-											) : this.props.plan[index].code[0] === 'd' ? (
-												<p>dinner</p>
-											) : this.props.plan[index].code[0] === 's' ? (
-												<p>snacks</p>
-											) : (
-												<p>what did you do</p>
-											)}
-											{this.props.plan[index].recipe.recipeName}
-											{this.props.plan[index].recipe.ingredient.map((ing) => (
-												<div>
-													<h1>ingredients</h1>
-													<div>{ing.name}</div>
-													<div>{ing.amount}</div>
-												</div>
-											))}
-											{this.props.plan[index].recipe.directions}
-										</div>
-									) : null}
-								</div>
-								{/* <div className='meal'>
+  render() {
+    // console.log('section props', this.props)
+    return (
+      <div className='sec-2'>
+        {this.props.plan.map((meal, index) => {
+          if (meal.code.includes(this.props.day)) {
+            return (
+              <section>
+                <div>
+                  {/* <p>breakfast</p> */}
+                  {this.props.plan[index].code[0] === "b" ? (
+                    <div className='meal'>
+                      {this.props.plan[index].code[0] === "b" ? (
+                        <p>breakfast</p>
+                      ) : this.props.plan[index].code[0] === "l" ? (
+                        <p>lunch</p>
+                      ) : this.props.plan[index].code[0] === "d" ? (
+                        <p>dinner</p>
+                      ) : this.props.plan[index].code[0] === "s" ? (
+                        <p>snacks</p>
+                      ) : (
+                        <p>what did you do</p>
+                      )}
+                      {this.props.plan[index].recipe.recipeName}
+                      {this.props.plan[index].recipe.ingredient.map(ing => (
+                        <div>
+                          <h1>ingredients</h1>
+                          <div>{ing.name}</div>
+                          <div>{ing.amount}</div>
+                        </div>
+                      ))}
+                      {this.props.plan[index].recipe.directions}
+                    </div>
+                  ) : this.props.plan[index].code[0] === "l" ? (
+                    <div className='meal'>
+                      {this.props.plan[index].code[0] === "b" ? (
+                        <p>breakfast</p>
+                      ) : this.props.plan[index].code[0] === "l" ? (
+                        <p>lunch</p>
+                      ) : this.props.plan[index].code[0] === "d" ? (
+                        <p>dinner</p>
+                      ) : this.props.plan[index].code[0] === "s" ? (
+                        <p>snacks</p>
+                      ) : (
+                        <p>what did you do</p>
+                      )}
+                      {this.props.plan[index].recipe.recipeName}
+                      {this.props.plan[index].recipe.ingredient.map(ing => (
+                        <div>
+                          <h1>ingredients</h1>
+                          <div>{ing.name}</div>
+                          <div>{ing.amount}</div>
+                        </div>
+                      ))}
+                      {this.props.plan[index].recipe.directions}
+                    </div>
+                  ) : this.props.plan[index].code[0] === "d" ? (
+                    <div className='meal'>
+                      {this.props.plan[index].code[0] === "b" ? (
+                        <p>breakfast</p>
+                      ) : this.props.plan[index].code[0] === "l" ? (
+                        <p>lunch</p>
+                      ) : this.props.plan[index].code[0] === "d" ? (
+                        <p>dinner</p>
+                      ) : this.props.plan[index].code[0] === "s" ? (
+                        <p>snacks</p>
+                      ) : (
+                        <p>what did you do</p>
+                      )}
+                      {this.props.plan[index].recipe.recipeName}
+                      {this.props.plan[index].recipe.ingredient.map(ing => (
+                        <div>
+                          <h1>ingredients</h1>
+                          <div>{ing.name}</div>
+                          <div>{ing.amount}</div>
+                        </div>
+                      ))}
+                      {this.props.plan[index].recipe.directions}
+                    </div>
+                  ) : this.props.plan[index].code[0] === "s" ? (
+                    <div className='meal'>
+                      {this.props.plan[index].code[0] === "b" ? (
+                        <p>breakfast</p>
+                      ) : this.props.plan[index].code[0] === "l" ? (
+                        <p>lunch</p>
+                      ) : this.props.plan[index].code[0] === "d" ? (
+                        <p>dinner</p>
+                      ) : this.props.plan[index].code[0] === "s" ? (
+                        <p>snacks</p>
+                      ) : (
+                        <p>what did you do</p>
+                      )}
+                      {this.props.plan[index].recipe.recipeName}
+                      {this.props.plan[index].recipe.ingredient.map(ing => (
+                        <div>
+                          <h1>ingredients</h1>
+                          <div>{ing.name}</div>
+                          <div>{ing.amount}</div>
+                        </div>
+                      ))}
+                      {this.props.plan[index].recipe.directions}
+                    </div>
+                  ) : null}
+                </div>
+                {/* <div className='meal'>
 									<p>lunch</p>
 									{this.props.plan[index].code[0] === 'l' ? (
 										<div>{this.props.plan[index].recipe.recipeName}</div>
@@ -123,13 +123,13 @@ class Section extends Component {
 										<div>{this.props.plan[index].recipe.recipeName}</div>
 									) : null}
 								</div> */}
-							</section>
-						)
-					}
-				})}
-			</div>
-		)
-	}
+              </section>
+            );
+          }
+        })}
+      </div>
+    );
+  }
 }
 
-export default Section
+export default Section;
